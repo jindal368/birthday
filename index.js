7f8fc48366ba647500e6453a4ed3862c606aac75
jindal368 <visheshjindal368@gmail.com>
2022-08-06 20:00:20 +0530

    1.4

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 5000 || process.env.PORT
 app.use('/style', express.static(__dirname + '/style'));
 app.use('/img', express.static(__dirname + '/img'));
 app.use('/script', express.static(__dirname + '/script'));
@@ -9,6 +8,6 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(port, () => {
+app.listen(5000 || process.env.PORT, () => {
     console.log(`Example app listening on port ${port}`)
 })
