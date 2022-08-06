377d9ffd204b17ba5e2f1517e90d32d080bdd004
jindal368 <visheshjindal368@gmail.com>
2022-08-06 19:55:08 +0530

    1.4

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000 || process.env.PORT
+const port = 5000 || process.env.PORT
 app.use('/style', express.static(__dirname + '/style'));
 app.use('/img', express.static(__dirname + '/img'));
 app.use('/script', express.static(__dirname + '/script'));
