9411aa001300b495312510db00952027b6db560f
jindal368 <visheshjindal368@gmail.com>
2022-08-06 19:34:44 +0530

    1.0

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use('/style', express.static(__dirname + '/style'));
+app.use('/img', express.static(__dirname + '/img'));
+app.use('/script', express.static(__dirname + '/script'));
+app.use('/customize.json', express.static(__dirname + '/customize.json'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
